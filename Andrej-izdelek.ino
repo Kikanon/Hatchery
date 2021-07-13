fcf664b205443a9903f693145f6d0ada1b4ac9c8
ElNarco <48151998+Kikanon@users.noreply.github.com>
2021-07-13 13:00:48 +0200

    Add files via upload
    Final

--- Andrej-izdelek.ino
+++ Andrej-izdelek.ino
@@ -4,44 +4,40 @@
 #include <DHT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <WiFi.h>
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 //i2c pins
 LiquidCrystal_I2C lcd(0x27, 16, 2); // 
 
-#define LcdSda 21 //lcd display
-#define LcdVcd 22
+#define LcdSda A4 //lcd display
+#define LcdVcd A5
 
-#define Senzor 27 //senzorji
-#define BackupSenzor 15
+#define Senzor 8 //senzorji
+#define BackupSenzor 9
 #define DHTTYPE DHT22
 DHT dht(Senzor, DHTTYPE);
 int chk; //idk
 
-#define Luc 33 //ogrevanje in indikacija
-#define redLed 26
-#define greLed 25
+#define Luc 10 //ogrevanje in indikacija
+#define redLed 11
+#define greLed 12
 
-#define servo 5 //nagib
+#define servo 3 //nagib
 
-#define t1 18 //tipki
-#define t2 19
+#define t1 7 //tipki
+#define t2 6
 
 //Vrednosti
-int settemp = 30; //nastavljena temparatura
+float settemp = 37; //nastavljena temparatura
 float temp = 0;//temparatura
 float temp2 = 0;//alarm
 float hum = 0;//vlaga
 bool heat = 0;
 int alarma = 0;
 int alc = 0;
+int showSet = 0;
 
-//Wifi
-const char* ssid     = "Valilnica";
-const char* password = "geslo123";
-WiFiServer server(80); //Port
-String header;
 
 //čas
 int currentMillis;
@@ -76,18 +72,10 @@ pinMode(Luc, OUTPUT);
 pinMode(redLed, OUTPUT);
 pinMode(greLed, OUTPUT);
 pinMode(servo, OUTPUT);
-pinMode(t1, INPUT_PULLDOWN);
-pinMode(t2, INPUT_PULLDOWN);
+pinMode(t1, INPUT_PULLUP);
+pinMode(t2, INPUT_PULLUP);
 
-//Wifi
-// Connect to Wi-Fi network with SSID and password
-  Serial.print("Setting AP (Access Point)…");
-  WiFi.softAP(ssid, password);
 
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("IP address: ");
-  Serial.println(IP);
-  server.begin();
 
 //bsenzor
 sensors.begin();
@@ -117,7 +105,7 @@ void loop() {
   Serial.print(temp2);
 setdata();
 sensor1();
-webpage();
+
 output();
 prikaz();
 delay(200); 
@@ -128,27 +116,18 @@ plin = digitalRead(t2);
 Serial.println("Senzor: ");
 Serial.print(plin);
   if(plin == false){ //false, ce je kliknjen, kr pac Arduino
-  if(plkl == 0){
-    settemp= settemp + 1;
-    plkl = 1;
-    }
-    Serial.println("plus kliknjen");
+    showSet = 4;
+    settemp= settemp + 0.5;
   }
-else{
-  plkl = 0;
-  Serial.println("plus ne kliknjen");
-  }
+
 //minus
 miin = digitalRead(t1);
   if(miin == false){ //false, ce je kliknjen, kr pac Arduino
-  if(mikl == 0){
-    settemp= settemp - 1;
+    showSet = 4;
+    settemp= settemp - 0.5;
     mikl = 1;
-    }
   }
-else{
-  mikl = 0;
-  }
+
   }
 
 void sensor1(){
@@ -174,57 +153,7 @@ void sensor1(){
   
   
   
-void webpage(){
-  WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client) {                             // If a new client connects,
-    //Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html><head><meta charset='utf-8'><title>Valilnica</title></head><body style='margin: 30px;'>");
-            client.println("<h1>Temparatura: ");
-            client.print(temp);
-            client.print("°C</h1><br>");
-            client.println("<h1>Vlažnost: ");
-            client.print(hum);
-            client.print(" %</h1></body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-  }
   
 void output(){
   //                                                                     Gretje
@@ -237,7 +166,7 @@ void output(){
   //                                                                     Varnost
   if(temp-temp2>=2.5||temp-temp2<=-2.5){   //
     alc++;
-    if(alc==5){
+    if(alc==1001){
     Serial.println("ALARM!!!!!!!");
     Serial.println("Temp: ");
     Serial.println(temp);
@@ -271,17 +200,19 @@ void output(){
     }
                                     //                                      REGULACIJA TEMPARATURE
    if(heat == 1){
-   digitalWrite(Luc, HIGH);
+   digitalWrite(Luc, LOW);
  //  Serial.println("GRETJE ON");
    }
    if(heat == 0){
-   digitalWrite(Luc, LOW);
+   digitalWrite(Luc, HIGH);
   // Serial.println("GRETJE OFF");
    }
   }
   
 void prikaz(){
-  
+
+  if(showSet <= 0){
+    lcd.clear();
   //Write your text:
 lcd.setCursor(0,0); //Prva vrstica
 lcd.print("Vlaga: "); //16 characters poer line
@@ -291,6 +222,16 @@ lcd.setCursor(0,1);
 lcd.print("Temp. :");//Druga vrstica
 lcd.print(temp);
 lcd.print(" C    ");
+
+  }
+  else{
+    lcd.clear();
+    lcd.setCursor(0,0); //Prva vrstica
+lcd.print("Set temp: "); //16 characters per line
+lcd.setCursor(0,1);
+lcd.print(settemp);
+showSet--;
+    }
 //                           GREEN LED
 digitalWrite(greLed, HIGH);
 delay(100);
